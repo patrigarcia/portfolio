6e6bb99041bdcfcf784486d0a64e780e23cd9ef5
Patricia Gonzalez <gonzalezgpatricia@hotmail.com>
2026-05-19 20:48:45 +0200

    Replace Vite setup with static site

--- src/react-app-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react-scripts" />
-
-declare namespace NodeJS {
-  interface ProcessEnv {
-    REACT_APP_EMAILJS_USER_ID: string;
-    REACT_APP_EMAILJS_SERVICE_ID: string;
-    REACT_APP_EMAILJS_TEMPLATE_ID: string;
-  }
-}
--- types/react-anchor-link-smooth-scroll/index.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'react-anchor-link-smooth-scroll';
